d067a80a843c5fa5ad4cda08193670293503918b
AlexKStein <aksteinphoto@gmail.com>
2022-08-01 14:33:38 -0400

    Spring set up, front end folder added

---
+++ BackEnd/src/test/java/WCCI/FinalProject/CookThis/ApplicationTests.java
@@ -0,0 +1,13 @@
+package WCCI.FinalProject.CookThis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
