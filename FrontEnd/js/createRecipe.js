c84260a0268f7ac694b553dee1c377e4cd78b70e
anathans1 <alexnathans1993@gmail.com>
2022-08-15 13:05:07 -0400

    new recipe dummy functionality in progress

--- FrontEnd/js/createRecipe.js
+++ FrontEnd/js/createRecipe.js
@@ -1,6 +1,6 @@
 import openBookTabs from "./components/openBookTabs.js"
-
-export default function newRecipeView(){
+let optionValue = 0
+export default function newRecipeView(categories){
 return `
     <div class="container">
         <div id="open-border">
@@ -13,14 +13,18 @@ return `
                             <h5>Recipe Name:</h5>
                                 <input id="recipe-name-in" type="text" placeholder="Ie. Captain Crunch Bars" class="form-control mb-3" aria-label="recipe name input" aria-describedby="inputGroup-sizing-default">
                             Ingredients & measurement:
+                            <div id="ingedient-in-flex">
                                 <div class="input-group">
-                                    <input id="ingredient-in" type="text" aria-label="Ingredient input" placeholder="ie. Lemon juice" class="form-control">
-                                    <input id="measurement-in" type="text" aria-label="Measurement input" placeholder="ie. 1 cup" class="form-control">
+                                    <input type="text" aria-label="Ingredient input" placeholder="ie. Lemon juice" class="form-control ingredient-in">
+                                    <input class="measurement-in form-control" type="text" aria-label="Measurement input" placeholder="ie. 1 cup">
                                 </div>
-                                <button class="btn btn-warning add-step-btn new-recipe-btns mb-3 align-self-start">Add Another Step</button>
+                            </div>
+                                <button class="btn btn-warning add-step-btn new-recipe-btns mb-3 align-self-start" id="add-ingredient-button">Add Another Ingredient</button>
                             Instructions:
-                                <input id="step-in" type="text" placeholder="Step 1..." class="form-control mb-1" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-                            <button class="btn btn-warning add-step-btn new-recipe-btns mb-3 align-self-start">Add Another Step</button>
+                            <div id="steps-in-flex">
+                                <input type="text" placeholder="Step 1..." class="form-control mb-1 step-in" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                            </div>
+                            <button class="btn btn-warning add-step-btn new-recipe-btns mb-3 align-self-start" id="add-step-button">Add Another Step</button>
                             Image:
                                 <input id="img-url-in" type="text" placeholder="Image URL" class="form-control mb-1" aria-label="image url input" aria-describedby="inputGroup-sizing-default">
                             <h6 class="text-center">or </h6>
@@ -29,7 +33,13 @@ return `
                                 <label class="input-group-text" for="category-in">Category:</label>
                                 <select class="form-select" id="category-in">
                                 <option selected>Select a category...</option>
-                                <option value="1">Food</option>
+                                ${categories.map(category => {
+                                    optionValue ++;
+                                    return `
+                                    <option value="${optionValue}">${category.title}</option>
+                                    `
+                                }).join("")
+                            }
                                 </select>
                             </div>
                             <br>
