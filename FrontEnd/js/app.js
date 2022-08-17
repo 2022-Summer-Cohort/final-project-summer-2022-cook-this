f22d1779a69bfa03fea9d7ba0c9d3f628ed8e8fe
ZakariyaMohamud <Zaklee5000@gmail.com>
2022-08-17 14:39:38 -0400

    SearchBar is done

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -61,20 +61,27 @@ function makeSearchView() {
 				recipes.map((recipe) => {
 					{
 						name: 'recipe.name';
+						picOfDish:'recipe.picOfDish'
+
 					}
 				})
 			];
+
 
 			function setList(group) {
 				const list = document.getElementById('listRecipes');
 				clearList();
 				for (const recipe of group) {
 					const item = document.createElement('a');
-					item.classList.add('recipe-list-item');
+					item.classList.add('recipe-list-item','d-flex','flex-column','justify-content-center');
+
 					const text = document.createTextNode(recipe.name);
+					const imageUrl = document.createElement("img");
+					imageUrl.src=recipe.picOfDish;
 					item.appendChild(text);
+					item.appendChild(imageUrl);
 					list.appendChild(item);
-                    list.addEventListener('click', () => {
+                    item.addEventListener('click', () => {
                         makeRecipeView(recipe.id);
                         
                     });
@@ -95,13 +102,16 @@ function makeSearchView() {
 				const item = document.createElement('a');
 				item.classList.add('recipe-list-item');
 				const text = document.createTextNode(recipeToSearch.name);
+				const imageUrl = document.createElement("img");
+				imageUrl.src=recipes.picOfDish;
 				item.appendChild(text);
+				item.appendChild(imageUrl);
 				list.appendChild(item);
 			}
 
 
 			const searchIN = document.getElementById('search777');
-
+			
 			searchIN.addEventListener('input', (event) => {
 				let value = event.target.value;
 				if (value && value.trim().length > 0) {
